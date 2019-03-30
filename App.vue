9e04211c2bfb964ae5719cda6eaa4a6f740b40a6
Ian Lim <mallim.ink@gmail.com>
2019-03-30 20:53:42 +0800

    initial commit

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+      Hello {{bundler}}
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Slide } from 'vue-burger-menu';
+
+export default{
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data: function(){
+    return {
+      bundler: "Parcel"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  color: green;
+}
+</style>
